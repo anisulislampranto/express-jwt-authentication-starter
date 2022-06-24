23f76748bb57b8567eddd2a5aa3fd6cdb565c68b
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-06-24 18:53:47 +0600

    implemented passport jwt strategy

--- config/passport.js
+++ config/passport.js
@@ -1,12 +1,49 @@
-const fs = require('fs');
-const path = require('path');
-const User = require('mongoose').model('User');
+const fs = require("fs");
+const path = require("path");
+const User = require("mongoose").model("User");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 
-const pathToKey = path.join(__dirname, '..', 'id_rsa_pub.pem');
-const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
+const pathToKey = path.join(__dirname, "..", "id_rsa_pub.pem");
+const PUB_KEY = fs.readFileSync(pathToKey, "utf8");
+
+// const passportJWTOptions = {
+//     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+//     secretOrKey: PUB_KEY || secret phrase,
+//     issuer: 'enter issuer here',
+//     audience: 'enter audience here',
+//     algorithms: ['RS256'],
+//     ignoreExpiration: false,
+//     passReqToCallback: false,
+//     jsonWebTokenOptions: {
+//         complete: false,
+//         clockTolerance: '',
+//         maxAge: '2d', // 2days,
+//         clockTimeStamp: '200',
+//         nonce: 'string here for openID'
+//     }
+// };
 
 // TODO
-const options = {};
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: PUB_KEY,
+  algorithms: ["RS256"],
+};
+
+const strategy = new JwtStrategy(options, (payload, done) => {
+  User.findOne({ _id: payload.sub })
+    .then((user) => {
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    })
+    .catch((err) => done(err, null));
+});
 
 // TODO
-module.exports = (passport) => {}
+module.exports = (passport) => {
+  passport.use(strategy);
+};
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,76 @@
-const mongoose = require('mongoose');
-const router = require('express').Router();   
-const User = mongoose.model('User');
-const passport = require('passport');
-const utils = require('../lib/utils');
+const mongoose = require("mongoose");
+const router = require("express").Router();
+const User = mongoose.model("User");
+const passport = require("passport");
+const utils = require("../lib/utils");
 
 // TODO
-router.get('/protected', (req, res, next) => {
+router.get(
+  "/protected",
+  passport.authenticate("jwt", { session: false }),
+  (req, res, next) => {
+    res.status(200).json({ success: true, msg: "You are authorized!" });
+  }
+);
+
+router.post("/login", function (req, res, next) {
+  User.findOne({ username: req.body.username })
+    .then((user) => {
+      if (!user) {
+        res.status(401).json({ success: false, msg: "could not find user" });
+      }
+
+      const isValid = utils.validPassword(
+        req.body.password,
+        user.hash,
+        user.salt
+      );
+
+      if (isValid) {
+        const tokenObject = utils.issueJWT(user);
+        res.status(200).json({
+          success: true,
+          user: user,
+          token: tokenObject.token,
+          expires: tokenObject.expires,
+        });
+      } else {
+        res
+          .status(401)
+          .json({ success: false, msg: "You entered the wrong password" });
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 // TODO
-router.post('/login', function(req, res, next){});
+router.post("/register", function (req, res, next) {
+  const saltHash = utils.genPassword(req.body.password);
 
-// TODO
-router.post('/register', function(req, res, next){});
+  const salt = saltHash.salt;
+  const hash = saltHash.hash;
+
+  const newUser = new User({
+    username: req.body.username,
+    hash: hash,
+    salt: salt,
+  });
+
+  newUser
+    .save()
+    .then((user) => {
+      const jwt = utils.issueJWT(user);
+
+      res.json({
+        success: true,
+        user: user,
+        token: jwt.token,
+        expiresIn: jwt.expires,
+      });
+    })
+    .catch((err) => next(err));
+});
 
 module.exports = router;
